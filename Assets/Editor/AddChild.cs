c4aaeb6ffee15ba3a67a16208de7d195c911b81c
zhangyi <zhangyi@V2005585-BJ>
2018-11-02 11:06:37 +0800

    添加 .gitignore 和 .gitattributes。
    Signed-off-by: zhangyi <zhangyi@V2005585-BJ>

---
+++ Assets/Editor/AddChild.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class AddChild : ScriptableObject
+{
+    [MenuItem("GameObject/+Add Child")]
+    static void MenuAddChild()
+    {
+        Transform[] transform = Selection.GetTransforms(SelectionMode.TopLevel|SelectionMode.OnlyUserModifiable);
+        foreach (Transform trans in transform)
+        {
+            GameObject newChild = new GameObject("_null");
+            newChild.transform.parent = trans;
+            newChild.transform.localPosition = Vector3.zero;
+        }
+    }
+
+    [MenuItem("GameObject/-Clear Child")]
+    static void MenuClearChild()
+    {
+        Transform[] transform = Selection.GetTransforms(SelectionMode.TopLevel | SelectionMode.OnlyUserModifiable);
+        foreach (Transform trans in transform)
+        {
+            if (trans.GetChild(0).name == "_null")
+            {
+                DestroyImmediate(trans.GetChild(0).gameObject);
+            }              
+        }
+    }	
+}
