058822fc6e9938a0ac692a020372dff2aef485c3
zhangyi <zhangyi@V2005585-BJ>
2018-12-18 17:09:58 +0800

    第一次提交

---
+++ Assets/C#/委托/event1/BaseUnit.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum NumType
+{
+   _hp,_mp,_exp
+}
+
+public class BaseUnit 
+{
+    private float _hp;
+    private float _mp;
+    private float _exp;
+    public string _name;
+	
+
+    public float hp
+    {
+        get
+        {
+            return _hp;
+        }
+    }
+
+    public float mp
+    {
+        get
+        {
+            return _mp;
+        }
+    }
+
+    public float exp
+    {
+        get
+        {
+            return _exp;
+        }
+    }
+
+    public BaseUnit(string name)
+    {
+        _name = name;
+        _hp = 100;
+        _mp = 100;
+        _exp = 0;
+    }
+
+    public delegate void changNum(BaseUnit source, float amount, NumType numType);
+    public event changNum OnNumChange;
+
+    public void ChangNum(float amount, NumType numType)
+    {
+        switch (numType)
+        {
+            case NumType._hp:
+                _hp += amount;
+                break;
+            case NumType._mp:
+                _mp += amount;
+                break;
+            case NumType._exp:
+                _exp += amount;
+                break;
+        }
+        if (OnNumChange != null)
+        {
+            OnNumChange(this,amount,numType);
+        }
+    }
+
+
+}
---
+++ Assets/C#/委托/event1/BattleInformation.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleInformation : MonoBehaviour {
+
+    BaseUnit hero;
+	// Use this for initialization
+	void Start ()
+    {
+        hero = new BaseUnit("MIKI");
+        hero.OnNumChange += Hero_OnNumChang;
+	}
+
+    private void Hero_OnNumChang(BaseUnit source, float amount, NumType numtype)
+    {
+        print(source._name+"__"+numtype.ToString()+"  "+"增加了:"+amount);
+    }
+
+    private void OnGUI()
+    {
+        if (GUI.Button(new Rect(0, 0, 100, 50), "加血"))
+        {
+            hero.ChangNum(100, NumType._hp);
+        }
+        if (GUI.Button(new Rect(0, 50, 100, 50), "补蓝"))
+        {
+            hero.ChangNum(50, NumType._mp);
+        }
+        if (GUI.Button(new Rect(0, 100, 100, 50), "经验"))
+        {
+            hero.ChangNum(88,NumType._exp);
+        }
+    }
+}
